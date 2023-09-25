f944e7aecfadb953d529151eee93683ab6f8e6cd
quyp <1095698022@qq.com>
2023-09-25 16:33:58 +0800

    feat: oh-my-live2d

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 // import TheWelcome from '../components/TheWelcome.vue'
+// import "oh-my-live2d/dist/oml-cubism2";
+// import { loadOhMyLive2D } from "oh-my-live2d";
+
+// loadOhMyLive2D({
+//   sayHello: false,
+//   transitionTime: 2000,
+//   models: {
+//     scale: 1.5,
+//   },
+//   // ...more
+// });
 </script>
 
 <template>
